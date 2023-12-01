70e23eff9c16515a02e3f897b24f2ea2aa9ea634
connoratrug <c.stroomberg@rug.nl>
2023-12-01 11:18:55 +0100

    day1 step 1 working

--- 00/00.ts
+++ 00/00.ts
@@ -1,6 +1,7 @@
 // deno run --allow-read
 
 import * as path from 'https://deno.land/std/path/mod.ts';
+import { readLines } from "https://deno.land/std@0.208.0/io/read_lines.ts";
 
 const aocNR = '00';
 
@@ -14,14 +15,44 @@ const dataFile = `./${aocNR}/${aocNR}.txt`;
 console.log("Running ", fullPath);
 console.log("Data file", dataFile);
 
-const decoder = new TextDecoder('utf-8');
+let total = 0;
+const f = await Deno.open(dataFile);
+for await (const l of readLines(f)) {
+    console.log("l:", l);
+    let number1 = 'x';
+    let index = 0;
+    while (number1 === 'x') {
+        const c = l.at(index)
+        if(c >= '0' && c <= '9') {
+            number1 = c;
+        } else {
+            index++;
+        }
+    }
 
-const text = Deno.readTextFileSync(dataFile);
-console.log(text);
+    index = l.length - 1;
+    let number2 = 'x';
+    while (number2 === "x") {
+        const c = l.at(index);
+        if (c >= "0" && c <= "9") {
+            number2 = c;
+        } else {
+            index--;
+        }
+    }
+    console.log("number1:", number1, "number2:", number2);
+    console.log("combi:", number1 + number2);
+    const lineSum = parseInt(number1 + number2);
+    total += lineSum;
+}
+
+console.log("total:", total);
+
+// console.log(text);
 
 // lambda function countWords
-const countWords = (s: string): number => s.split(/\s+/g).filter(w => /[a-z0-9]/.test(w)).length;
-const count = countWords(text);
-console.log(`File ${dataFile} has ${count} words.`);
+// const countWords = (s: string): number => s.split(/\s+/g).filter(w => /[a-z0-9]/.test(w)).length;
+// const count = countWords(text);
+// console.log(`File ${dataFile} has ${count} words.`);
 
-console.log(text.replaceAll("\n",' '));
+// console.log(text.replaceAll("\n",' '));
