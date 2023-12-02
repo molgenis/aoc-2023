5d219d0c3a4da54a5888320edaff8d0108727922
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2023-12-02 08:55:59 +0100

    Not completely finished 01

--- clemens/01/01.ts
+++ clemens/01/01.ts
@@ -1,23 +1,15 @@
-#!/ deno run --allow-read
+#!/usr/bin/env deno run --allow-read
 
 import * as path from 'https://deno.land/std/path/mod.ts';
 
 const aocNR = '01';
 
-const fullPath:string = import.meta.url;
-const __dirname = path.dirname(fullPath);
-const __filename = path.basename(fullPath);
+const dataPath = `./clemens/${aocNR}`;
 
-// const dataFile = `${__dirname}/${aocNR}.txt`;
-const dataFile = `./clemens/${aocNR}/${aocNR}.txt`;
+console.log("Puzzel: ", path.basename(dataPath));
 
-console.log("Running ", fullPath);
-console.log("Data file", dataFile);
-
-const decoder = new TextDecoder('utf-8');
-
-const DEBUG=true;
-const MODE=2;
+const DEBUG = true;
+const MODE = 2;
 
 const print = (s:string,i:number) => {
     if (MODE !== i) return;
@@ -25,79 +17,37 @@ const print = (s:string,i:number) => {
     console.log(s)
 }
 
-const p1 = (s:string) => print(s, 1);
-const p2 = (s:string) => print(s, 2);
-
-let input = Deno.readTextFileSync(dataFile);
-
-let text = input;
-// console.log(text);
-let re = /[a-z]/gi;
-
-let sum = 0;
-text.split('\n').forEach(line => {
-    line = line.replace(re, "");
-    let num = parseInt(line[0]+line[line.length-1]);
-    sum += num
-    p1(`${num} ${line[0]+line[line.length-1]}`);
-});
-console.log("v1", sum);
-
-sum = 0;
-const digits = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-
-text = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
+let input = Deno.readTextFileSync(`${dataPath}/01.txt`);
+let t1 = Deno.readTextFileSync(`${dataPath}/t1.txt`);
+let t2 = Deno.readTextFileSync(`${dataPath}/t2.txt`);
 
 
-function replaceFirstOccurrence(text:string) {
-
-    text.split('\n').forEach(line => {
-        const origLine = line;
-        p2(origLine);
-    
-        // replace first occurrece of digit
-        let minKey = "";
-        do {
-            let minValue = Infinity;
-            minKey = "";
-        
-            digits.forEach((digit) => {
-                const index = line.indexOf(digit);
-                if (index> -1) {
-                    if ( index < minValue) {
-                        minValue = index;
-                        minKey = digit;
-                    }
-                }
-            });
-
-            if (minKey !== "") {
-                p2(`  ${minKey} : ${minValue} : ${line}`);
-                const digitVal = digits.indexOf(minKey) + 1;
-                p2(`  ${minKey} : ${digitVal}`);
-                line = line.replace(minKey, `${digitVal}`);
-            }
-
-        } while (minKey !== "")
-
-        line = line.replace(re, "");
-        let num = parseInt(line[0]+line[line.length-1]);
-        p2(`    ${origLine} -> ${num}`);
-        sum += num;
-        p2(`    ${num} : ${line[0]+line[line.length-1]}`);
-    });
-    // 53652 too low
-    // 54110 too high
-    console.log("v2", sum);
+const p1 = (inp:string) => {
+    const replaceLetters = /[a-z]/gi;
+    return inp.split('\n').map((v,_i,_a) => { 
+    const clean = v.replace(replaceLetters, '');
+    return parseInt(clean[0] + clean[clean.length-1]);
+}).reduce((p,c) => p + c, 0);
 }
 
-//replaceFirstOccurrence(text);
+if (MODE==1) {
+    console.log(`t1 ${p1(t1)}`);
+    console.log(`v1 ${p1(input)}`);
+}
+const digits = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let digitAndDigitWord = digits.map((v,i) => `${i+1}|${v}`).join('|');
+console.log(digitAndDigitWord);
 
-const replaceLetters = /[a-z]/gi;
-text = input;
-const v1 = text.split('\n').map((v,_i,_a) => { 
-    const clean = v.replace(replaceLetters, '');
-    const result = clean[0] + clean[clean.length-1];
-    return parseInt(result);
-}).reduce((p,c) => p + c, 0);
-console.log(`v: ${v1}`);
+const p2 = (inp:string) => {
+    const replaceLetters = RegExp(`(${digitAndDigitWord})`, "gi");
+    return inp.split('\n').map((v,_i,_a) => { 
+        const matched = v.match(replaceLetters);
+        const fromWord = (v) => {if (digits.indexOf(v) > -1) v = digits.indexOf(v)+1; return v;}
+        const f = fromWord(matched[0]);
+        const l = fromWord(matched[matched.length-1]);
+        return parseInt(`${f}${l}`);
+    }).reduce((p,c) => p + c, 0);
+}
+console.log(`t2 ${p2(t2)}`);
+console.log(`v2 ${p2(input)}`);
+
