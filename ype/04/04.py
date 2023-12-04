ba7b76aa3479d90753b4a5603a43fde5f3d23958
Ype Zijlstra <info@ypezijlstra.frl>
2023-12-04 14:01:38 +0100

    Added solution to part two of puzzle 4

--- ype/04/04.py
+++ ype/04/04.py
@@ -11,8 +11,8 @@ class PuzzleSolver:
         """Initializes the object by reading in the file."""
         self.data = self._read_input(filename)
 
-    def solve(self, show_matching: bool = False) -> int:
-        """Solves the puzzle based on input."""
+    def solve_part_1(self, show_matching: bool = False) -> int:
+        """Solves the first part of the puzzle."""
         if show_matching:
             print("Matching numbers:")
         scores = {}
@@ -25,13 +25,34 @@ class PuzzleSolver:
             if show_matching:
                 print(f"{card:3}: {winning_nums}")
 
-        print("The scores of each card equals: ")
+        print("The score of each card equals: ")
         for card, score in scores.items():
             print(f"{card:3}: {score}")
 
         print(f"The total score equals: {sum(scores.values())}")
 
         return sum(scores.values())
+
+    def solve_part_2(self) -> int:
+        """Solves the second part of the puzzle."""
+        scores = {
+            card: len(list(set(numbers['win_nums']) & set(numbers['my_nums'])))
+            for card, numbers in self.data.items()
+        }
+
+        copies = {card: 1 for card in scores.keys()}
+
+        for card, s in scores.items():
+            if s == 0:
+                continue
+            c = copies[card]
+            for i in range(1, s+1):
+                copies[card+i] += c
+
+        cards_total = sum(copies.values())
+        print(f"Total number of cards: {cards_total}.")
+        
+        return cards_total
 
     @staticmethod
     def _read_input(fn: str) -> dict:
@@ -41,7 +62,7 @@ class PuzzleSolver:
 
         cards = {}
         for line in raw_data:
-            card_idx = line.split('Card ')[1].split(':')[0]
+            card_idx = int(line.split('Card ')[1].split(':')[0])
             win_nums = [int(num) for num in line.split(': ')[1].split(' |')[0].split()]
             my_nums = [int(num) for num in line.split('| ')[1].split()]
             cards.update({card_idx: {'win_nums': win_nums, 'my_nums': my_nums}})
@@ -52,8 +73,10 @@ class PuzzleSolver:
 if __name__ == '__main__':
     test_file = 't1.txt'
     test_solver = PuzzleSolver(test_file)
-    test_solution = test_solver.solve()
+    test_solution_1 = test_solver.solve_part_1()
+    test_solution_2 = test_solver.solve_part_2()
 
     real_file = '04.txt'
     real_solver = PuzzleSolver(real_file)
-    real_solution = real_solver.solve(show_matching=True)
+    real_solution_1 = real_solver.solve_part_1()
+    real_solution_2 = real_solver.solve_part_2()
