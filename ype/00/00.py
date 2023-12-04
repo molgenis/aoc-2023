e8846e272bd1bf850b7b3fe59b067e02d6c6d70d
Ype Zijlstra <info@ypezijlstra.frl>
2023-12-04 14:50:50 +0100

    Added solutions to puzzle 1 and template for other solutions

---
+++ ype/00/00.py
@@ -0,0 +1,38 @@
+"""
+Script for solving the zeroth puzzle of the Advent of Code 2023 competition.
+https://adventofcode.com/2023/day/0
+"""
+
+
+class PuzzleSolver:
+    """Object to solve a puzzle."""
+
+    def __init__(self, filename: str):
+        """Initializes the object by reading in the file."""
+        self.data: list = self._read_input(filename)
+
+    def solve_part_1(self) -> int:
+        """Solves the first part of the puzzle."""
+
+    def solve_part_2(self) -> int:
+        """Solves the second part of the puzzle."""
+
+    @staticmethod
+    def _read_input(fn: str) -> list:
+        """Reads in the txt file and returns the list of cards."""
+        with open(file=fn) as f:
+            raw_data = f.readlines()
+
+        return raw_data
+
+
+if __name__ == '__main__':
+    test_file = 't1.txt'
+    test_solver = PuzzleSolver(test_file)
+    test_solution_1 = test_solver.solve_part_1()
+    test_solution_2 = test_solver.solve_part_2()
+
+    real_file = '00.txt'
+    real_solver = PuzzleSolver(real_file)
+    real_solution_1 = real_solver.solve_part_1()
+    real_solution_2 = real_solver.solve_part_2()
---
+++ ype/01/01.py
@@ -0,0 +1,64 @@
+"""
+Script for solving the first puzzle of the Advent of Code 2023 competition.
+https://adventofcode.com/2023/day/1
+"""
+
+
+class PuzzleSolver:
+    """Object to solve a puzzle."""
+
+    def __init__(self, filename: str):
+        """Initializes the object by reading in the file."""
+        self.data = self._read_input(filename)
+
+    def solve_part_1(self) -> int:
+        """Solves the first part of the puzzle."""
+        calibration_values = []
+        for line in self.data:
+            digits = [d for d in line if d.isdigit()]
+            cal_val = int(digits[0] + digits[-1])
+            calibration_values.append(cal_val)
+        print(f"Sum of all calibration values: {sum(calibration_values)}")
+
+        return sum(calibration_values)
+
+    def solve_part_2(self) -> int:
+        """Solves the second part of the puzzle."""
+        nums = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+                'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+        calibration_values = []
+        for line in self.data:
+            digits = []
+            for i, d in enumerate(line):
+                if d.isdigit():
+                    digits.append(d)
+                elif any(line[i:].startswith(n) for n in nums.keys()):
+                    digits.extend(str(nums[n]) for n in nums.keys() if line[i:].startswith(n))
+
+            cal_val = int(digits[0] + digits[-1])
+            calibration_values.append(cal_val)
+        print(f"Sum of all calibration values: {sum(calibration_values)}")
+
+        return sum(calibration_values)
+
+    @staticmethod
+    def _read_input(fn: str) -> list:
+        """Reads in the txt file and returns the list of cards."""
+        with open(file=fn) as f:
+            raw_data = f.readlines()
+
+        return raw_data
+
+
+if __name__ == '__main__':
+    test1_file = 't1.txt'
+    test1_solver = PuzzleSolver(test1_file)
+    test1_solution_1 = test1_solver.solve_part_1()
+    test2_file = 't2.txt'
+    test2_solver = PuzzleSolver(test2_file)
+    test_solution_2 = test2_solver.solve_part_2()
+
+    real_file = '01.txt'
+    real_solver = PuzzleSolver(real_file)
+    real_solution_1 = real_solver.solve_part_1()
+    real_solution_2 = real_solver.solve_part_2()
