04827a0351cb8e85a0b94634c79f53abeb676f7d
Ype Zijlstra <y.zijlstr@gmail.com>
2023-12-07 12:55:35 +0100

    Solved 7-2

--- ype/07/07.py
+++ ype/07/07.py
@@ -29,25 +29,8 @@ class PuzzleSolver:
             # Count the occurrences of each strength in the hand and sort the occurrences in descending order
             ch_counts = sorted(map(lambda ch: hand.count(ch), set(hand)), reverse=True)
 
-            # Using these counts, class the hand as one of the seven types (7: five of a kind, ..., 0: high card)
-            if ch_counts[0] > 3:
-                # Five or four of a kind
-                h_type = ch_counts[0]+1
-            elif ch_counts[0] == 3:
-                # Three of a kind
-                h_type = 3
-                if ch_counts[1] == 2:
-                    # Full house
-                    h_type += 1
-            elif ch_counts[1] == 2:
-                # Two pair
-                h_type = 2
-            elif ch_counts[0] == 2:
-                # One pair
-                h_type = 1
-            else:
-                # High card
-                h_type = 0
+            # Using these counts, class the hand as one of the seven types (6: five of a kind, ..., 0: high card)
+            h_type = self._find_hand_type(ch_counts)
 
             # Create a unique score code for each card, consisting of the hand type value and the value of each card
             score_code = str(h_type) + ''.join(map(lambda card:
@@ -68,7 +51,69 @@ class PuzzleSolver:
         return winnings
 
     def solve_part_2(self) -> int:
-        """Solves the second part of the puzzle."""
+        """Solves the second part of the puzzle.
+
+        The reasoning is as follows:
+        Note that when a hand contains a joker, it is always optimal to count
+        it as the card that already has the most occurrences.
+        The same logic and procedures for solving part 1 apply.
+        """
+
+        # Set the new value for J
+        self.strengths.update({'J': 1})
+
+        # Find the types and create a score code for each hand
+        hand_types = {}
+        for hand, bid in self.hands.items():
+            # Count the occurrences of each strength in the hand and sort the occurrences in descending order
+            # Add each occurrence of the J to the highest number of occurrences
+            ch_counts = sorted(map(lambda ch: hand.replace('J', '').count(ch), set(hand)), reverse=True)
+            j_count = hand.count('J')
+            ch_counts[0] += j_count
+
+            # Using these counts, class the hand as one of the seven types (6: five of a kind, ..., 0: high card)
+            h_type = self._find_hand_type(ch_counts)
+
+            # Create a unique score code for each card, consisting of the hand type value and the value of each card
+            score_code = str(h_type) + ''.join(map(lambda c:
+                                                   str(self.strengths.get(hand[c], hand[c])).rjust(2, '0'),
+                                                   range(len(hand))))
+            hand_types[score_code] = int(bid)
+
+        # Sort the bids based on the hand scores
+        ordered_bids = {key: hand_types.copy()[key] for key in sorted(hand_types.keys())}
+
+        # Tally the winnings of the bids
+        winnings = 0
+        for rank, bid in zip(range(1, len(ordered_bids)+1), ordered_bids.values()):
+            winnings += rank * bid
+
+        print(f"The total winnings are {winnings}.")
+
+        return winnings
+
+    @staticmethod
+    def _find_hand_type(card_counts: list) -> int:
+        """Returns the numerical value of the type of the hand."""
+        if card_counts[0] > 3:
+            # Five or four of a kind
+            h_type = card_counts[0] + 1
+        elif card_counts[0] == 3:
+            # Three of a kind
+            h_type = 3
+            if card_counts[1] == 2:
+                # Full house
+                h_type += 1
+        elif card_counts[1] == 2:
+            # Two pair
+            h_type = 2
+        elif card_counts[0] == 2:
+            # One pair
+            h_type = 1
+        else:
+            # High card
+            h_type = 0
+        return h_type
 
     @staticmethod
     def _read_input(fn: str) -> dict:
